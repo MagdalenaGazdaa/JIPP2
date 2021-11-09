c38a0b7a2cd0b9be68127291b1cf7f71e875eb45
MagdalenaGazdaa <91936355+MagdalenaGazdaa@users.noreply.github.com>
2021-11-09 08:30:51 +0100

    Add files via upload

---
+++ lab5/zad1.2/zad1/cmake-build-debug/CMakeFiles/e1NoCopyingConstructor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/e1NoCopyingConstructor.dir/src/main.cpp.o"
+  "CMakeFiles/e1NoCopyingConstructor.dir/src/Point.cpp.o"
+  "e1NoCopyingConstructor.pdb"
+  "e1NoCopyingConstructor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/e1NoCopyingConstructor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
